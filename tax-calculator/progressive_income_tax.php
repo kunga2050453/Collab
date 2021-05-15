1963aa62c727352e155f9f447a2eb78c8a9eb6ac
Swarin Shakya <np03a190281@heraldcollege.edu.np>
2021-05-15 20:25:17 +0545

    Added dropdown menus to select gender and marital status to calculate annual tax.

--- tax-calculator/progressive_income_tax.php
+++ tax-calculator/progressive_income_tax.php
@@ -1,7 +1,13 @@
 <?php
 	$grossIncome = filter_input(INPUT_POST, 'grossIncome',
         FILTER_VALIDATE_FLOAT);
-
+    if(isset($_POST['formSubmit']) )
+    {
+      $varGender = $_POST['formGender'];
+      $errorMessage = ""; 
+    }
+    $state = filter_input(INPUT_POST, 'formGender',
+        FILTER_VALIDATE_FLOAT);
     // validate grossIncome
     if ($grossIncome === FALSE ) {
         $error_message = 'Investment must be a valid number.'; 
@@ -16,7 +22,7 @@
 
      // if an error message exists, go to the form page
     if ($error_message != '') {
-        include('form.php');
+        include('taxcalculator.php');
         exit(); 
     }
 
@@ -24,24 +30,76 @@
 
     // calculate taxable income, which is gross income minus 10,000
 	$taxableIncome = $grossIncome - 0;
-
 	/* use conditionals to decide tax rate, perform computation to get
 	total income tax */
-	if ($taxableIncome <= 400000) {
-		$totalIncomeTax = ($taxableIncome * 1) / 100;
-	}
-	elseif ($taxableIncome >= 400001 && $taxableIncome <= 500000) {
-		$totalIncomeTax = ((($taxableIncome - 400000) * 10) / 100) + 4000;
-	}
-	elseif ($taxableIncome >= 500001 && $taxableIncome <= 600000) {
-		$totalIncomeTax = ((($taxableIncome - 500000) * 20) / 100) + 14000;
-	}
-	elseif ($taxableIncome >= 600001 && $taxableIncome <= 1500000) {
-		$totalIncomeTax = ((($taxableIncome - 600000) * 30) / 100) + 54000; 
-	}
-	else {
-		$totalIncomeTax = ((($taxableIncome - 1500000) * 36) / 100) + 444000;
-	}
+    if ($_POST["formGender"] == "M" && $_POST["formState"] == "M") {
+        if ($taxableIncome <= 450000) {
+            $totalIncomeTax = ($taxableIncome * 1) / 100;
+        }
+        elseif ($taxableIncome >= 450001 && $taxableIncome <= 550000) {
+            $totalIncomeTax = ((($taxableIncome - 400000) * 10) / 100) + 4500;
+        }
+        elseif ($taxableIncome >= 550001 && $taxableIncome <= 750000) {
+            $totalIncomeTax = ((($taxableIncome - 500000) * 20) / 100) + 14500;
+        }
+        elseif ($taxableIncome >= 750001 && $taxableIncome <= 2000000) {
+            $totalIncomeTax = ((($taxableIncome - 600000) * 30) / 100) + 54500; 
+        }
+        else {
+            $totalIncomeTax = ((($taxableIncome - 1500000) * 36) / 100) + 429500;
+        }
+    }
+    elseif ($_POST["formGender"] == "M" && $_POST["formState"] == "U") {
+        if ($taxableIncome <= 400000) {
+            $totalIncomeTax = ($taxableIncome * 1) / 100;
+        }
+        elseif ($taxableIncome >= 400001 && $taxableIncome <= 500000) {
+            $totalIncomeTax = ((($taxableIncome - 400000) * 10) / 100) + 4000;
+        }
+        elseif ($taxableIncome >= 500001 && $taxableIncome <= 600000) {
+            $totalIncomeTax = ((($taxableIncome - 500000) * 20) / 100) + 14000;
+        }
+        elseif ($taxableIncome >= 600001 && $taxableIncome <= 2000000) {
+            $totalIncomeTax = ((($taxableIncome - 600000) * 30) / 100) + 54000; 
+        }
+        else {
+            $totalIncomeTax = ((($taxableIncome - 1500000) * 36) / 100) + 444000;
+        }
+    }
+    elseif ($_POST["formGender"] == "F" && $_POST["formState"] == "M") {
+        if ($taxableIncome <= 450000) {
+            $totalIncomeTax = ($taxableIncome * 1) / 100;
+        }
+        elseif ($taxableIncome >= 450001 && $taxableIncome <= 550000) {
+            $totalIncomeTax = ((($taxableIncome - 400000) * 10) / 100) + 4500;
+        }
+        elseif ($taxableIncome >= 550001 && $taxableIncome <= 750000) {
+            $totalIncomeTax = ((($taxableIncome - 500000) * 20) / 100) + 14500;
+        }
+        elseif ($taxableIncome >= 750001 && $taxableIncome <= 2000000) {
+            $totalIncomeTax = ((($taxableIncome - 600000) * 30) / 100) + 54500; 
+        }
+        else {
+            $totalIncomeTax = ((($taxableIncome - 1500000) * 36) / 100) + 429500;
+        }
+    }
+    else {
+        if ($taxableIncome <= 400000) {
+            $totalIncomeTax = ($taxableIncome * 1) / 100;
+        }
+        elseif ($taxableIncome >= 400001 && $taxableIncome <= 500000) {
+            $totalIncomeTax = ((($taxableIncome - 400000) * 10) / 100) + 4000;
+        }
+        elseif ($taxableIncome >= 500001 && $taxableIncome <= 600000) {
+            $totalIncomeTax = ((($taxableIncome - 500000) * 20) / 100) + 14000;
+        }
+        elseif ($taxableIncome >= 600001 && $taxableIncome <= 2000000) {
+            $totalIncomeTax = ((($taxableIncome - 600000) * 30) / 100) + 54000; 
+        }
+        else {
+            $totalIncomeTax = ((($taxableIncome - 1500000) * 36) / 100) + 444000;
+        }
+    }
 
 	// calculate effectiveTaxRate and store 
 	$effectiveTaxRate = $totalIncomeTax / $grossIncome;
@@ -59,12 +117,12 @@
 <html>
 <head> 
 	<meta charset="utf-8" />
-	<title> Progressive Income Tax </title>
+	<title> Income Tax </title>
 	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
 	<main> 
-		<h1> Progressive Income Tax Calculator </h1> 
+		<h1> Tax Calculator </h1> 
 		<?php
 		echo "As a single taxpayer with a gross annual income of Rs. " . $grossIncome_f
 			 . ", your taxable income is Rs. " . $taxableIncome_f . ".";
---
+++ tax-calculator/taxcalculator.php
@@ -0,0 +1,55 @@
+<?php
+  // set default value of variable for initial page load 
+    if (!isset($grossIncome)) { $grossIncome = ''; }
+    if (!isset($state)) { $state = ''; }
+    
+?> 
+
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title> Tax Calculator </title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<body> 
+	<main> 
+    <h1> Tax Calculator </h1> 
+     <?php if (!empty($error_message)) { ?>
+        <p class="error"><?php echo htmlspecialchars($error_message); ?></p>
+    <?php } ?>
+
+	<form action="progressive_income_tax.php" method="post">
+
+		<div id="data">
+            <div class="dropdown">
+                <select name="formGender" class="form-control">
+                  <option value="">Gender</option>
+                  <option value="M">Male</option>
+                  <option value="F">Female</option>
+                </select>
+                <select name="formState" class="form-control">
+                  <option value="">Marital Status</option>
+                  <option value="M">Married</option>
+                  <option value="U">Unmarried</option>
+                </select>
+            </div>
+            <div class="range">
+                <input type="range" value="<?php echo htmlspecialchars($grossIncome); ?>" class="slider" name="grossIncome" min="0" max="5000000000">
+            </div>
+            <br>
+			<label> Income: </label>
+			<input type="text" name="grossIncome" 
+		       value="<?php echo htmlspecialchars($grossIncome); ?>">
+		    <br>
+		</div>
+		<div id = "buttons">
+			<label>&nbsp;</label>
+        	<input type="submit" value="Calculate">
+        	<br>
+        </div>
+	</form>
+	</main>
+</body>
+</html> 
